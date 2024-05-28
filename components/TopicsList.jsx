71f6d86dd22a07c76dc1be592312f613549245d8
Muhammad Umair <meumairkhan47@gmail.com>
2024-05-28 13:24:41 +0500

    updated topicList file

--- components/TopicsList.jsx
+++ components/TopicsList.jsx
@@ -1,58 +1,44 @@
-"use client";
-
-import React, { useEffect, useState } from "react";
+import React from "react";
 import RemoveBtn from "./RemoveBtn";
 import Link from "next/link";
 import { HiPencilAlt } from "react-icons/hi";
 
 const getTopics = async () => {
   try {
-    const res = await fetch("http://localhost:3000/api/topics");
+    const res = await fetch("http://localhost:3000/api/topics", {
+      cache: "no-store",
+    });
     if (!res.ok) {
       throw new Error("Failed to fetch topics");
     }
     return res.json();
   } catch (error) {
     console.log("Error loading topics", error);
-    return { topics: [] }; // Return a default value to prevent undefined
   }
 };
 
-const TopicsList = () => {
-  const [topics, setTopics] = useState([]);
-
-  useEffect(() => {
-    const fetchTopics = async () => {
-      const data = await getTopics();
-      setTopics(data.topics || []); // Handle undefined or missing topics
-    };
-    fetchTopics();
-  }, []);
-
+const TopicsList = async () => {
+  const { topics } = await getTopics();
   return (
-    <div>
-      {topics.length > 0 ? (
-        topics.map((t) => (
-          <div
-            key={t._id}
-            className="p-4 border border-slate-300 my-3 flex justify-between gap-5 items-start bg-black bg-opacity-75 text-white"
-          >
-            <div>
-              <h2 className="font-bold text-2xl">{t.title}</h2>
-              <div>{t.description}</div>
-            </div>
-            <div className="flex gap-2">
-              <RemoveBtn id={t._id} />
-              <Link href={`/editTopic/${t._id}`}>
-                <HiPencilAlt size={24} />
-              </Link>
-            </div>
+    <>
+      {topics.map((t) => (
+        <div
+          key={t._id}
+          className="p-4 border border-slate-300 my-3 flex justify-between gap-5 items-start bg-black bg-opacity-75 text-white"
+        >
+          <div>
+            <h2 className="font-bold text-2xl">{t.title}</h2>
+            <div>{t.description}</div>
           </div>
-        ))
-      ) : (
-        <div>No topics available</div>
-      )}
-    </div>
+          <div className="flex gap-2">
+            <RemoveBtn id={t._id} />
+            <Link href={`/editTopic/${t._id}`}>
+              <HiPencilAlt size={24} />
+            </Link>
+          </div>
+        </div>
+      ))}
+    </>
   );
 };
 
